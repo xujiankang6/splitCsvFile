e7f5312346ab8f9a1dfd04907a0c8cf479772816
xujiankang6 <1241450485@qq.com>
2020-02-20 11:49:10 +0800

    提交第一版代码

---
+++ src/main/java/com/forcecloud/splitfile/SplitfileApplication.java
@@ -0,0 +1,37 @@
+package com.forcecloud.splitfile;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
+@SpringBootApplication
+public class SplitfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitfileApplication.class, args);
+    }
+
+
+    /**
+     * 设置文件上传大小
+     *
+     * @return
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大
+        factory.setMaxFileSize(DataSize.ofMegabytes(8000)); //MB
+        //factory.setMaxFileSize(DataSize.ofKilobytes(80)); //KB
+        //factory.setMaxFileSize(DataSize.ofGigabytes(80)); //Gb
+
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize(DataSize.ofMegabytes(1000000));
+        return factory.createMultipartConfig();
+    }
+}
---
+++ src/test/java/com/forcecloud/splitfile/SplitfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forcecloud.splitfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
