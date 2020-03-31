08088af885b619bbce9258c2a1c56cb541c61bdf
xujiankang6 <1241450485@qq.com>
2020-03-31 17:21:56 +0800

    修改splitFileCtrl，使其支持excel文件拆分

--- src/main/java/com/jiankang/splitfile/controller/SplitFileCtrl.java
+++ src/main/java/com/jiankang/splitfile/controller/SplitFileCtrl.java
@@ -4,11 +4,6 @@ package com.jiankang.splitfile.controller;
 import com.jiankang.splitfile.utils.ExportZipUtils;
 import com.jiankang.splitfile.utils.ForceDeleteFilesUtils;
 import com.jiankang.splitfile.utils.SplitUtils;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.util.CellRangeAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,15 +13,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 
 @Controller
@@ -34,6 +25,9 @@ import java.io.OutputStream;
 public class SplitFileCtrl {
 
     private static Logger logger = LoggerFactory.getLogger(SplitFileCtrl.class);
+    public static final String CSV = "csv";
+    public static final String XLS = "xls";
+    public static final String XLSX = "xlsx";
 
     @Autowired
     SplitUtils splitUtils;
@@ -53,24 +47,35 @@ public class SplitFileCtrl {
             InputStream inputStream = file.getInputStream();
             String originalFilename = file.getOriginalFilename();
             String[] split = originalFilename.split("\\.");
-            String type = split[split.length - 1];
+            String fileType = split[split.length - 1];
             int index = originalFilename.lastIndexOf(".");
             String fileName = originalFilename.substring(0, index);
             int splitSize = Integer.valueOf(size);
             logger.info("文件信息：文件名：{}  文件类型： {}" +
-                            "  文件大小：{}MB   要求拆分文件最大行数： {}", fileName, type,
+                            "  文件大小：{}MB   要求拆分文件最大行数： {}", fileName, fileType,
                     file.getSize() / 1024 / 1024, splitSize);
-            String zipPath = SplitUtils.getCsvZipPath(inputStream, fileName, splitSize);
-            exportZipUtils.zipExport(zipPath, request, response);
-            forceDeleteFilesUtils.deleteAllFilesOfDir(new File(zipPath));
+            //拆分后的文件路径
+            String splitFilePath = "";
+            switch (fileType) {
+                case CSV:
+                    splitFilePath = SplitUtils.getCsvZipPath(inputStream, fileName, splitSize);
+                    break;
+                case XLS:
+                    splitFilePath = SplitUtils.getXlsZipPath(inputStream, fileName, splitSize);
+                    break;
+                case XLSX:
+                    splitFilePath = SplitUtils.getXlsxZipPath(inputStream, fileName, splitSize);
+                    break;
+                default:
+                    return "fail";
+            }
+            exportZipUtils.zipExport(splitFilePath, request, response);
+            forceDeleteFilesUtils.deleteAllFilesOfDir(new File(splitFilePath));
         } catch (Exception e) {
             e.printStackTrace();
         }
         return "success";
     }
-
-
-
 
 
 }
